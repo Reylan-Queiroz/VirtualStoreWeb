af8ed56ddf40a31f5dcd6f9a4847a1923567cf25
Willian Lima <william_slayer@hotmail.com>
2022-04-08 15:43:37 -0300

    Validação do formulario

--- src/app/pages/retaguarda/pessoa/cadastro-pessoa/cadastro-pessoa.component.ts
+++ src/app/pages/retaguarda/pessoa/cadastro-pessoa/cadastro-pessoa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validator, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-pessoa',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroPessoaComponent implements OnInit {
 
-  constructor() { }
+   formCadPessoa!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
 
   ngOnInit(): void {
+    this.formCadPessoa = this.fb.group({
+      tipoCadastro: ['', Validators.compose([
+         Validators.required
+      ])],
+      familia: [''],
+      cpf: ['', Validators.required],
+      rg: ['', Validators.required],
+      sexo: ['', Validators.required],
+      nome: ['', Validators.compose([
+         Validators.required
+      ])],
+      apelido: ['', Validators.compose([
+         Validators.maxLength(40)
+      ])],
+      dataNacimento: ['',Validators.compose([
+
+      ])],
+      endereco: ['', Validators.compose([
+         Validators.required
+      ])],
+      numero: ['', Validators.compose([
+         Validators.required
+      ])],
+      pontoRef: ['', Validators.compose([
+         Validators.required,
+         Validators.maxLength(50)
+      ])],
+      bairro: ['', Validators.compose([
+         Validators.required,
+         Validators.maxLength(30)
+      ])],
+      cep: ['', Validators.compose([
+         Validators.required
+      ])],
+      uf: ['',Validators.required],
+      cidade: ['',Validators.required],
+      residente: ['',],
+      comercial: [''],
+      celular1: [''],
+      celular2: [''],
+      Vendedor: [''],
+      crediario: [''],
+      limiteCred: [''],
+      observacao: ['']
+    })
   }
 
 }
