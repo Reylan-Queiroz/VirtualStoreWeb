686ae6e6f23d2c006909a3062592a78f168c33a0
Reylan-Queiroz <reylanqueiroz@gmail.com>
2022-04-12 09:32:06 -0300

    Implementando Authorized.guard (Já funcionando)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthorizedGuard } from './core/guards/authorized.guard';
 
 const routes: Routes = [
    { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
-   { path: 'produto/cadastro', loadChildren: () => import('./pages/retaguarda/produto/cadastro-produto/cadastro-produto.module').then(m => m.CadastroProdutoModule) },
+   { path: 'produto/cadastro', canActivate: [AuthorizedGuard], loadChildren: () => import('./pages/retaguarda/produto/cadastro-produto/cadastro-produto.module').then(m => m.CadastroProdutoModule) },
    //{ path: 'pessoa/cadastro-pessoa', loadChildren: () => import('./pages/retaguarda/pessoa/cadastro-pessoa/cadastro-pessoa.module').then(m => m.CadastroPessoaModule) }
 ];
 
